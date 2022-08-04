c6c3ec41acc7d173ed116bbeb30125f1895950c0
nikkojustix <nickolaykotov@gmail.com>
2022-08-05 01:07:18 +0300

    first commit

---
+++ src/components/modal/modal.js
@@ -0,0 +1,26 @@
+// инициализация
+export function bindModal(trigger, modal, close) {
+  trigger = document.querySelector(trigger);
+  modal = document.querySelector(modal);
+  close = document.querySelector(close);
+
+  const body = document.body;
+  trigger.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.style.display = 'flex';
+    body.classList.add('locked');
+  });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    body.classList.remove('locked');
+  });
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+      body.classList.remove('locked');
+    }
+  });
+}
+
+// вызов
+flsFunctions.bindModal('trigger-selector', 'modal-selector', 'close-selecor');
