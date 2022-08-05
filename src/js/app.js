2c3cebc2859d5b265175e10854c65d50c7be26e7
nikkojustix <nickolaykotov@gmail.com>
2022-08-05 15:26:59 +0300

    added header with menu and modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,24 @@
 import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
+
+// mobile menu
+const menuBtn = document.querySelector('.menu-btn');
+menuBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const menu = document.querySelector('.menu');
+  const menuInner = document.querySelector('.menu__inner');
+  document.body.classList.add('locked');
+  menu.classList.add('menu--opened');
+  menuInner.classList.add('menu__inner--opened');
+  menu.addEventListener('click', (e) => {
+    if (e.target === menu) {
+      document.body.classList.remove('locked');
+      menu.classList.remove('menu--opened');
+      menuInner.classList.remove('menu__inner--opened');
+    }
+  });
+});
+
+// modals
+flsFunctions.bindModal('.header__callback', '.modal--callback', '.modal__close');
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -4,8 +4,7 @@ export function isWebp() {
     webP.onload = webP.onerror = function () {
       callback(webP.height == 2);
     };
-    webP.src =
-      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
   }
 
   testWebP(function (support) {
@@ -16,5 +15,28 @@ export function isWebp() {
     // } else {
     //   document.querySelector('body').classList.add('no-webp');
     // }
+  });
+}
+
+export function bindModal(trigger, modal, close) {
+  trigger = document.querySelector(trigger);
+  modal = document.querySelector(modal);
+  close = document.querySelector(close);
+
+  const body = document.body;
+  trigger.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.style.display = 'flex';
+    body.classList.add('locked');
+  });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    body.classList.remove('locked');
+  });
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+      body.classList.remove('locked');
+    }
   });
 }
